40b9770eba9cb8a6163c680b5fea46e64cf5d1f9
Martin <joewwi03@gmail.com>
2023-05-22 23:22:27 +0300

    Completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
   # write code here
+  def initialize l1,l2,l3
+    @sides = [l1, l2, l3]
+  end
+
+  def kind
+    if @sides.any? { |side| side <= 0 } || @sides.sum - @sides.max <= @sides.max
+      raise TriangleError, "Invalid triangle sides"
+    elsif @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+     
+  end
+
 end
